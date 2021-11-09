5eb8bdf10a3594e8fb2856cebac59f025973feb0
LouisFitdevoie <fitdevoielouis@hotmail.be>
2021-11-09 15:56:34 +0100

    Premier commit

---
+++ EC-configuration/src/main/java/be/heh/ec/EcConfigurationApplication.java
@@ -0,0 +1,13 @@
+package be.heh.ec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcConfigurationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcConfigurationApplication.class, args);
+    }
+
+}
---
+++ adapters/EC-persistence/src/test/java/be/heh/ec/EcPersistenceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.heh.ec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcPersistenceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
